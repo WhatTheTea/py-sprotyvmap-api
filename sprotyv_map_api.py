f04c4836f62e2ec723b9d09b0c2b69da9997c6a8
WhatTheTea <fox228.af@gmail.com>
2023-02-17 20:52:40 +0200

    raw milcoms

--- sprotyv_map_api.py
+++ sprotyv_map_api.py
@@ -1,7 +1,13 @@
 import flask
+import sprotyv_parser as parser
 
 app = flask.Flask(__name__)
 
-@app.route("/")
-def say_hi():
-    return "<h1>hi</h1>"
+@app.route("/milcoms/raw/get")
+def get_raw_milcoms():
+    result = []
+    milcoms = parser.get_milcoms()
+    for m in milcoms:
+        result.extend(m)
+        result.append("\n")
+    return "".join(result)
--- sprotyv_parser.py
+++ sprotyv_parser.py
@@ -2,11 +2,26 @@ from collections import namedtuple
 from lxml import etree
 import requests
 
-# Структура з даними з сайту
+# Структура з даними з сайту 
+# MilCom - military commissariat (військомат)
 milcom_raw = namedtuple('MilComRaw', 'name info phones')
 # Завантаження дерева сайту
-sprotyv_html = requests.get("https://sprotyv.in.ua/")
+sprotyv_html = requests.get("https://sprotyv.in.ua/").text
 sprotyv_tree = etree.HTML(sprotyv_html)
 # TODO: Error handling
 
-for obl in range(0,24):
+# XPath потрібних даних
+# .format: 1 - область, 2 - військомат, 3 - стовпець
+xpath = "/html/body/div/section[2]/div/div[{}]/div/div[2]/div/div/table/tbody/tr[{}]/td[{}]/text()" 
+
+def get_milcoms() -> list:
+    # Парсинг даних з сайту
+    milcoms_raw = []
+    for i_obl in range(1,24+1):
+        for i_milcom in range(1,2+1): #TODO: N of milcoms
+            name = sprotyv_tree.xpath(xpath.format(i_obl, i_milcom, 1))[0]
+            info = sprotyv_tree.xpath(xpath.format(i_obl, i_milcom, 2))[0]
+            phones = sprotyv_tree.xpath(xpath.format(i_obl, i_milcom, 3))[0]
+            milcoms_raw.append(milcom_raw(name, info, phones))
+    return milcoms_raw
+
