b7d7f022c03875fb18949ea6e9ac850a1d6451a7
WhatTheTea <silchenko.whatthetea@gmail.com>
2023-11-07 12:34:36 +0200

    [refactoring] json

--- sprotyvmap_api/data/preprocessing.py
+++ sprotyvmap_api/data/preprocessing.py
@@ -1,8 +1,7 @@
 from data.Point import Point
 from requests import get as http_get
-from typing import Dict,List,Tuple
+from typing import Dict, List,Tuple
 from lxml import etree
-import re
 
 # Завантаження дерева сайту
 sprotyv_html = http_get("https://sprotyv.in.ua/").text
@@ -24,9 +23,9 @@ def point(district_id: int, point_id: int) -> Point:
     Номер військкомата є відносним кожної окремої області.\n
     Args:
         district_id (int): номер області (1..24)
-        milcom_id (int): номер військкомату (1..n)
+        TODO
     Returns: 
-        MilComRaw: "сирі" дані про військкомат з адресою замість координат.
+        TODO
     """
     data = []
     for i in range(3):
@@ -35,32 +34,19 @@ def point(district_id: int, point_id: int) -> Point:
         data.append(cleanstr(raw))
     return Point(*data)
 
-def district(district_id:int) -> Tuple[str, List[Point]]:
+def district(district_id:int):
     """
     Повертає ім'я області та військкомати в ній\n
     Args:
         district_id (int): номер області (1..24)
     Returns:
-        Tuple[str, List[MilComRaw]]: кортеж з імені області та списку "сирих" військкоматів
+        TODO
     """
     table = sprotyv_tree.xpath(XPATH_TABLE.format(district_id))[0].getchildren()
     district_name = "".join(sprotyv_tree.xpath(XPATH_DISTRICT.format(district_id))).strip()
     points_count = len(table) - 1
-    points = [point(district_id, milcom_id) for milcom_id in range(1,points_count)]
+    points = [point(district_id, point_id) for point_id in range(1,points_count)]
     
-    return (district_name, points)
-
-def all_districts() -> Dict[str, List[Point]]:
-    """
-    Повертає список спарсених адрес військкоматів розділених по областям\n
-    Returns:
-        Dict[str, List[MilComRaw]]: словник ім'я області : список "сирих" військкоматів
-    """
-    districts_raw = dict()
-    for district_id in range(1,24+1):
-        # DistrictTuple: 0 - name; 1 - milcoms
-        district_tuple = district(district_id)
-        districts_raw[district_tuple[0]] = district_tuple[1]
-    return districts_raw
+    return {'district':district_name, 'points':points}
 
     
--- sprotyvmap_api/json_converter.py
+++ sprotyvmap_api/json_converter.py
@@ -16,9 +16,6 @@ def point(district_id:int, point_id:int):
         data = json.dumps(point)
         return data
 
-def all_districts():
-    return districts_generator()
-
 
 def district(district_id:int):
     """
@@ -29,46 +26,14 @@ def district(district_id:int):
     Returns:
         flask.Response : HTTP відповідь з JSON даними про військкомати в окремій області
     """
-    name, points = dp.district(district_id)
-    data = points_generator(points)
-    if data:
-        result = "{"
-        result += f'"{name}":'
-        result += json.dumps(data)
-        result += '}'
-        return result
+    district = dp.district(district_id)
+    points = [point.asdict() for point in district['points'] if point]
+    return json.dumps({
+        'district': district['district'],
+        'points': points
+        })
 
-def districts_generator():
-        """
-        Генерує дані про військкомати у форматі:
-        { "district":[...], "other":[...] }
+def all_districts():
+    data = [data.append(district(i)) for i in range(1,24+1)]
+    return data
         
-        Returns: 
-            Generator[str, None, None] : Генератор потокових даних про всі військкомати України
-        """
-        yield '{'
-        # Отримання "сирих" військкоматів
-        districts = list(dp.all_districts().items())
-
-        for i in range(len(districts)):
-            # Розпаковка області
-            name, milcoms_raw = districts[i]
-
-            yield f'"{name}":'
-            milcoms = points_generator(milcoms_raw)
-            yield json.dumps(milcoms)
-            if i < len(districts)-1:
-                yield ","
-        yield '}'
-
-def points_generator(points:List[Point]) -> List[dict]:
-    """
-    Обробляє спарсені військкомати та фільтрує від пустих словників
-
-    Args:
-        milcoms_raw (List[MilComRaw]) : "сира" інформація про військкомати для обробки
-    Returns:
-        List[dict] : Представлення об'єктів MilCom у вигляді словників
-    """
-    return [point.asdict() for point in points if point]
-
